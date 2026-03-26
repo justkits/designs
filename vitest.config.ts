6775b7a046675be946e6a34674f6584b3607354c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-03-26 12:31:09 +0900

    rollback vitest config change, and modify ci command instead

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from "vitest/config";
 
-const isCI = process.env.CI === "true";
-
 export default defineConfig({
   test: {
     projects: ["apps/*", "packages/*"],
     coverage: {
-      include: isCI ? ["**/*/src/**/*.{ts,tsx}"] : undefined,
       exclude: [
         "apps/*/src/**/index.ts",
         "apps/*/src/**/main.ts",
@@ -17,7 +14,7 @@ export default defineConfig({
         "**/*/tests/*",
       ],
       provider: "v8",
-      reporter: isCI ? ["clover"] : [["text", { skipFull: true }]],
+      reporter: [["text", { skipFull: true }]],
     },
   },
 });
