217da5d9cfc568c521ca218755ab954e1df76d04
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2026-04-08 20:41:42 +0900

    minor fix

--- apps/designs/app/page.tsx
+++ apps/designs/app/page.tsx
@@ -1,19 +1,18 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="page">
+      <main className="main">
         <Image
-          className={styles.logo}
+          className="logo"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
-        <div className={styles.intro}>
+        <div className="intro">
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
@@ -35,15 +34,15 @@ export default function Home() {
             center.
           </p>
         </div>
-        <div className={styles.ctas}>
+        <div className="ctas">
           <a
-            className={styles.primary}
+            className="primary"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className={styles.logo}
+              className="logo"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
@@ -52,7 +51,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className={styles.secondary}
+            className="secondary"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
